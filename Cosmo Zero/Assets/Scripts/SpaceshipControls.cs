d48db1401a8764631002022ce5dfafdcd5416b0a
RedShift <william.lin@epita.fr>
2017-01-15 00:08:08 +0100

    Added first script

---
+++ Cosmo Zero/Assets/Scripts/SpaceshipControls.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceshipControls : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float maxSpeed = 100;
+    public float minSpeed = -100;
+
+    public float RotationSpeed = 100;
+
+    public float acceleration = 1;
+    public float deceleration = 1;
+    public float thrust = 0;
+    
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    rb = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+	    if ()
+	    {
+	        
+	    }
+
+	    float forwardBackward = Input.;
+
+
+	    if ( && thrust > 0)
+	    {
+	        thrust -= deceleration;
+	    }
+        else if ( && thrust < 0)
+        {
+            thrust += deceleration;
+        }
+        Debug.Log("thrust: " + thrust);
+	}
+}
