8d1b5d2708667581f91ea10146a857415cb31e47
Maëlle <maelle.roubeau@epita.fr>
2017-01-31 16:19:45 +0100

    Missiles 0.0.1 just started
    Added Shooting ability to the player, + created some kind of missile
    prefab. Follow the direction and orientation of the player. Don't
    disappear.  Don't hurt. Don't do anything really.

---
+++ Cosmo Zero/Assets/Scripts/Bulette.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bulette : MonoBehaviour {
+
+    private float creation;
+    private Rigidbody rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        creation = Time.time;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.up * 20);
+    }
+}
---
+++ Cosmo Zero/Assets/Scripts/Shooting.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour {
+
+    private Rigidbody rb;
+    public Rigidbody bullet;
+    public float cd;
+    public int damage;
+    private float timeavl; //Moment at which next shoot will be available;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        timeavl = Time.time + cd;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if ((Input.GetKeyDown(KeyCode.P)) & Time.time >= timeavl)
+        {
+            timeavl = Time.time + cd;
+            Shoot();
+        }
+	}
+    void Shoot()
+    {
+        Rigidbody test = Instantiate(bullet);
+        float z1 = rb.transform.position.z + 1;
+        Vector3 place = new Vector3(rb.transform.position.x, rb.transform.position.y, z1);
+        test.transform.position = place;
+        test.transform.rotation = rb.transform.rotation;
+        Vector3 rot = new Vector3(90, 0, 0);
+        test.transform.Rotate(rot);
+        Object.Destroy(test, 2.0f);
+    }
+}
