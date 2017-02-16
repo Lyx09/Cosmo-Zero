370db11da398597a6823a8408c9b2775544b0a7a
Kenan -POI- Lejosne <kenan.lejosne@gmail.com>
2017-02-16 11:54:14 +0100

    merge-M_shooting : merging by hand M_shooting
    Listing the changes here, the gitignore according to issue #8
    Changes to be committed:
    	modified:   .gitignore
    	new file:   Cosmo Zero/Assets/Bullets.prefab
    	new file:   Cosmo Zero/Assets/Scripts/BadTest.cs
    	new file:   Cosmo Zero/Assets/Scripts/Bulette.cs
    	new file:   Cosmo Zero/Assets/Scripts/Shooting.cs
    	new file:   Cosmo Zero/Assets/Scripts/State.cs
    	new file:   Cosmo Zero/Assets/TestEnemy.prefab

---
+++ Cosmo Zero/Assets/Scripts/Bulette.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bulette : MonoBehaviour {
+
+    private float creation;
+    private Rigidbody rb;
+    private GameObject sender;
+    public Vector3 speed;
+    // Use this for initialization
+    public void SetSender(GameObject go)
+    {
+        sender = go;
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        creation = Time.time;
+        rb.AddForce(transform.up * 50);
+    }
+
+    void Update()
+    {
+        if (Time.time > creation + 1.0F)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = speed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            gameObject.SetActive(false);
+            BadTest enemysc = other.GetComponent<BadTest>();
+            if (enemysc.life <= 1)
+            {
+                State shooter = sender.GetComponent<State>();
+                shooter.Kill();
+            }
+            enemysc.Hurt(1);
+            Object.Destroy(gameObject);
+        }
+    }
+}
---
+++ Cosmo Zero/Assets/Scripts/Shooting.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public GameObject bullet;
+    public float cd;
+    public int damage;
+    private float timeavl; //Moment at which next shoot will be available;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        timeavl = Time.time + cd;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if ((Input.GetKeyDown(KeyCode.P)) & Time.time >= timeavl)
+        {
+            timeavl = Time.time + cd;
+            Shoot();
+        }
+	}
+    void Shoot()
+    {
+        GameObject test = Instantiate(bullet);
+        test.transform.position = gameObject.transform.position;
+        test.transform.rotation = rb.transform.rotation;
+        test.transform.Rotate(90, 0, 0);
+        /*float deadZoneRadius = Screen.width / 25.0F; //relative to screen width
+        float disToCenX = Input.mousePosition[0] - Screen.width / 2F;
+        float disToCenY = Input.mousePosition[1] - Screen.height / 2F;
+        Vector3 disToCen = new Vector3(disToCenX, disToCenY, 0);
+        float rotateOnXAxis = -disToCenY * 2F / Screen.height;
+        float rotateOnYAxis = disToCenX * 2F / Screen.width;
+        test.transform.Rotate(rb.transform.rotation.x * rotateOnXAxis * 1000, rb.transform.rotation.y * rotateOnYAxis * 1000, 0);*/
+        Rigidbody rb2 = test.GetComponent<Rigidbody>();
+        Bulette bull = test.GetComponent<Bulette>();
+        Vector3 speed = new Vector3();
+        speed = (rb.velocity * 10) / 6 + transform.forward * 20;
+        bull.speed = speed;
+        rb2.velocity = speed;
+        bull.SetSender(gameObject);
+    }
+}
