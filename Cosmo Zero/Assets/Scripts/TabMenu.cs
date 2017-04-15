86863b07a7f3ad0c6e1d5b9cb62927e7390f83cf
Maëlle <maelle.roubeau@epita.fr>
2017-04-15 22:11:32 +0200

    Tuto step 1.5 back to the future
    Did shit on the last commit. Tried to fix. Please work
    +~ Dialogue, basis for tutorial
    "Tutorial" is the main scene and "actual tutorial" is the tutorial, of
    couuuuuuuuuurse

---
+++ Cosmo Zero/Assets/Scripts/TabMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TabMenu : MonoBehaviour
+{
+    public GameObject Panel;
+    public GameObject Disp;
+	// Use this for initialization
+	void Start ()
+    {
+        Panel.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!Panel.gameObject.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                Panel.gameObject.SetActive(true);
+                Disp.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            if (Input.GetKeyUp(KeyCode.Tab))
+            {
+                Panel.gameObject.SetActive(false);
+                Disp.gameObject.SetActive(true);
+            }
+        }
+	}
+}
---
+++ Cosmo Zero/Assets/Scripts/TutoQuests.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TutoQuests : MonoBehaviour
+{
+    private int step;
+    public GameObject Dialogue;
+    public Text message;
+
+    void Start ()
+    {
+        Dialogue.gameObject.SetActive(true);
+        step = 0;
+	}
+	
+	void Update ()
+    {
+	    switch (step)
+        {
+            case 0:
+                message.text = "Hello, \nWelcome to the Cosmo Zero Tutorial!\n";
+                if (Input.anyKeyDown)
+                    step = 1;
+                break;
+            case 1:
+                message.text = "You're going to learn the basics of the game";
+                break;
+            default:
+                break;
+        }
+	}
+}
