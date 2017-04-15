2a328a2abee4c75bd014ae9a75df933834f627f1
Maëlle <maelle.roubeau@epita.fr>
2017-04-15 18:11:16 +0200

    1st step Tutorial: Tab Menu
    Pressing Tab while in tutorial displays the commands (and soon the
    current task). Yeah. That's all I've got

---
+++ Cosmo Zero/Assets/Scripts/TabMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TabMenu : MonoBehaviour
+{
+    public GameObject Canvas;
+    public GameObject Display;
+    void Start ()
+    {
+        Canvas.gameObject.SetActive(false);
+	}
+	
+	void Update ()
+    {
+	    if (Canvas.gameObject.activeSelf)
+        {
+            if (Input.GetKeyUp(KeyCode.Tab))
+            {
+                Canvas.gameObject.SetActive(false);
+                Display.gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                Canvas.gameObject.SetActive(true);
+                Display.gameObject.SetActive(false);
+            }
+        }
+	}
+}
