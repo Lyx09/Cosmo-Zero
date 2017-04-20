a96d72d3775f14db07d98bf22f3f5c7f6694e0a2
Maëlle <maelle.roubeau@epita.fr>
2017-04-20 15:16:37 +0200

    Homing missile 1st version (already satisfying hehe) Added pack Rockets Created missile prefab Created Missile Script Added all that stuff to the Ship Prefab Linear trajectory, slow movement, not that bad

---
+++ Assets/Scripts/MissileBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileBehaviour : MonoBehaviour
+{
+    public Transform target;
+    public GameObject sender;
+    public Vector3 speed;
+    private Rigidbody rb;
+    public int dmg;
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetComponent<Rigidbody>().transform.LookAt(target);
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = rb.transform.forward * 50;
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            BadTest enemysc = other.gameObject.GetComponent<BadTest>();
+            if (enemysc.life <= dmg)
+            {
+                State shooter = sender.GetComponent<State>();
+                shooter.Kill(enemysc.xp);
+            }
+            enemysc.Hurt(dmg);
+        }
+    }
+}
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -9,6 +9,11 @@ public class Shooting : MonoBehaviour
     public float cd;
     public int damage;
     private float timeavl; //Moment at which next shoot will be available;
+    public GameObject Missile;
+    public static Transform target;
+    public float missilecd;
+    public int missiledmg;
+    public float missileavl;
     private AudioSource audioshoot;
 
 	// Use this for initialization
@@ -16,18 +21,25 @@ public class Shooting : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         timeavl = Time.time + cd;
+        missileavl = Time.time + missilecd;
         audioshoot = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetButton("Fire1") & (Time.time >= timeavl))
+        if (Input.GetButton("Fire1") && (Time.time >= timeavl))
         {
             timeavl = Time.time + cd;
             audioshoot.Stop();
             audioshoot.Play();
             Shoot();
+        }
+        if (Input.GetMouseButtonDown(1) && (Time.time >= missileavl) && GetComponent<Lock>().target != null)
+        {
+            target = GetComponent<Lock>().target;
+            missileavl = Time.time + missilecd;
+            SendMissile();
         }
 	}
     void Shoot()
@@ -50,5 +62,20 @@ public class Shooting : MonoBehaviour
         bull.speed = speed;
         rb2.velocity = speed;
         bull.SetSender(gameObject);
+    }
+    void SendMissile()
+    {
+        GameObject mymissile = Instantiate(Missile);
+        mymissile.transform.position = gameObject.transform.position + gameObject.transform.forward;
+        mymissile.transform.LookAt(target);
+        MissileBehaviour mb = mymissile.GetComponent<MissileBehaviour>();
+        Rigidbody msrb = mymissile.GetComponent<Rigidbody>();
+        mb.target = target;
+        mb.sender = gameObject;
+        Vector3 missilespeed = new Vector3();
+        missilespeed = msrb.transform.forward * 50;
+        msrb.velocity = missilespeed;
+        mb.speed = missilespeed;
+        mb.dmg = missiledmg;
     }
 }
