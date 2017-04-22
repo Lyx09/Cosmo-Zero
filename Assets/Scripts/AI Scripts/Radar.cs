8635523a69a6fc30e872d5922710c5997e68d6fe
RedShift <william.lin@epita.fr>
2017-04-22 17:12:28 +0200

    Nearly working radar

--- Assets/Scripts/AI Scripts/Radar.cs
+++ Assets/Scripts/AI Scripts/Radar.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Radar : MonoBehaviour
 {
@@ -9,20 +10,64 @@ public class Radar : MonoBehaviour
     private float radius_radar = 20F;
     [SerializeField]
     private GameObject blip;
+    private Transform disc;
 
-    private Transform blips;
+    private List<Transform> list_radar;
+    private List<Transform> list_actual;
 
-    void Start () {
-	
-	}
+    void Start ()
+    {
+        list_radar = new List<Transform>();
+        list_actual = new List<Transform>();
+
+        disc = transform.transform.Find("disc");
+    }
 	
 	void Update ()
 	{
-	    Collider[] insideRadar = Physics.OverlapSphere(radar_center.position,radius_radar);
-	    foreach (Collider collider1 in insideRadar)
+	    int layerMask = 1 << 8; //Layer 8
+        Collider[] insideRadar = Physics.OverlapSphere(radar_center.position,radius_radar,layerMask);
+
+	    foreach (Collider collider1 in insideRadar) //add new objects in the radar
 	    {
-	        Instantiate(blip, blips);
+	        bool add = true;
+	        int counter = 0;
+	        while (counter < list_actual.Count && add) //Test if collider is already in the radar
+	        {
+	            if (list_actual[counter].name == collider1.name) //maybe find another unique property
+	            {
+                    add = false;
+	            }
+	            counter++;
+	        }
+
+	        if (add) //if it is not, adds it
+	        {
+                Vector3 radar_pos = transform.position + (collider1.transform.position - radar_center.position) * transform.localScale.x / radius_radar;
+                
+                GameObject new_obj= Instantiate(blip, radar_pos, disc.rotation) as GameObject;
+	            new_obj.transform.parent = gameObject.transform;
+
+                
+                list_actual.Add(collider1.transform);
+                list_radar.Add(new_obj.transform);
+            }
+	    }
+        
+
+	    for (int i = 0; i < list_radar.Count; i++) //Checks if all the objects in the radar are still i the radar
+	    {
+	        if ((list_actual[i].position - radar_center.position).magnitude > radius_radar)
+	        {
+	            Destroy(list_radar[i].gameObject); //Destroy the blip if no more in the radar
+	            list_actual.RemoveAt(i);
+	            list_radar.RemoveAt(i);
+            }
+        }
+	    for (int i = 0; i < list_radar.Count; i++) //updates the elements in the radar
+	    {
+	        list_radar[i].localPosition = (list_actual[i].localPosition - radar_center.localPosition) * transform.localScale.x /radius_radar; //* transform.localScale.x is used to put in the radar sphere
 	    }
 
-	}
+    }
 }
