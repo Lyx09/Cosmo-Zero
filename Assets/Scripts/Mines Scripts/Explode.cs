f0a835211c6c5fbc97081fb394313b40b56e1be7
Hafaku <theau.degroote@epita.fr>
2017-04-24 11:14:00 +0200

    DL mines asset + fix emplacement of mine's script

---
+++ Assets/Scripts/Mines Scripts/Explode.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Explode : MonoBehaviour {
+
+
+    public float radius = 20f;
+    public float minespeed = 0.01f;
+    public float dammages = 150f;
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position , radius);
+        
+        float mindist = radius;
+        if (hitColliders.Length != 0)
+        {
+            foreach(Collider c in hitColliders)
+            {
+                if (c.tag == "Player" || c.tag == "Enemy")
+                {
+                    Transform closest_target = null;
+                    if (mindist > (c.transform.position - transform.position).magnitude)
+                    {
+                        mindist = (c.transform.position - transform.position).magnitude;
+                        closest_target = c.transform;
+                    }
+                    Vector3 direction = (closest_target.position - transform.position).normalized * Mathf.Abs((closest_target.position - transform.position).magnitude - radius) * minespeed;
+                    rb.AddForce(direction);
+                }                
+            }            
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("touchaient");
+        Destroy(gameObject);
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Enemy")
+        {
+            State state = collision.gameObject.GetComponent<State>();
+            state.Hurt(dammages);
+        }
+    }
+}
