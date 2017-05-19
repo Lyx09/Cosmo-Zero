90b00dc768edbd09b9a142133fdba349e57e4480
Maëlle <maelle.roubeau@epita.fr>
2017-05-19 18:01:52 +0200

    ADDED HEALTH BAR BECAUSE WILLIAM IS USELESS. And even made it transparent ;) + new script that will handle its filling

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+    public Image image;
+	// Use this for initialization
+	void Start ()
+    {
+        image = GetComponent<Image>();
+        Color c = image.color;
+        c.a = 0.5F;
+        image.color = c;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
