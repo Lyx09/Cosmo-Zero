f3149ba65cf4668488a6aadf85aa85f4487c40b8
Maëlle <maelle.roubeau@epita.fr>
2017-05-20 14:22:16 +0200

    Working script health bar Single color. Not smooth. Need to re-add numbers for life money and exp. Need to handle opacity better

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -2,19 +2,24 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class HealthBar : MonoBehaviour {
-    public Image image;
+public class HealthBar : MonoBehaviour
+{
+    public GameObject player;
+    public Image background;
+    public Image foreground;
+    private State s;
 	// Use this for initialization
 	void Start ()
     {
-        image = GetComponent<Image>();
-        Color c = image.color;
-        c.a = 0.5F;
-        image.color = c;
+        Color c = background.color;
+        c.a = 0.3F;
+        background.color = c;
+        s = player.GetComponent<State>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        foreground.fillAmount = s.life / s.maxlife;
 	}
 }
