c029cca189b1fe2426880e2f9f4cb13b0fe96ce3
Maëlle <maelle.roubeau@epita.fr>
2017-06-03 21:28:00 +0200

    First seconds Solo game: Alarm -Added Menu displaying current quests -Added alarm sound The game opens with a black screen, and an alarm

--- Assets/Scripts/TutoMenu.cs
+++ Assets/Scripts/TutoMenu.cs
@@ -2,7 +2,7 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class TabMenu : MonoBehaviour
+public class TutoMenu : MonoBehaviour
 {
     public GameObject Panel;
     public GameObject Disp;
