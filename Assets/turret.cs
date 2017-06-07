7780b9789de2d9931df269359262db2ddceb37de
RedShift <william.lin@epita.fr>
2017-06-07 05:03:25 +0200

    trying to code turret

--- Assets/turret.cs
+++ Assets/turret.cs
@@ -9,6 +9,7 @@ public class turret : MonoBehaviour
     public float hp = 5;
     public float rotation_speed = 1;
     public Shooting shooting;
+    public Transform rcOrigin;
 
     void Start()
     {
@@ -36,15 +37,16 @@ public class turret : MonoBehaviour
 	        }
 	        else
 	        {
-                Ray ray = new Ray(transform.position + transform.forward,target.transform.position);
-	            RaycastHit hit;
+                Ray ray = new Ray(rcOrigin.position,target.transform.position - rcOrigin.position);
+                Debug.DrawLine(rcOrigin.position, target.transform.position);
+                RaycastHit hit;
 	            if (Physics.Raycast(ray, out hit, detection_radius))
 	            {
-	                Debug.DrawLine(ray.origin, hit.point);
+                    Debug.DrawLine(ray.origin, hit.point,Color.cyan);
 	                if (hit.collider.tag == "Player")
 	                {
-	                    transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.LookRotation(target.transform.position), rotation_speed * Time.deltaTime);
-                        shooting.Shoot();
+	                    transform.rotation = Quaternion.Slerp(transform.localRotation, Quaternion.LookRotation(target.transform.position), rotation_speed * Time.deltaTime);
+                        //shooting.Shoot();
 	                }
                 }
 	        }
